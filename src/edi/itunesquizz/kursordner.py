dad1761c942eeff978f3a87b6855a8ed3cf6ee87
educorvi GmbH Educorvi Co. KG <educorvi@astra1607.startdedicated.de>
2018-10-22 00:41:26 +0200

    Änderungen

--- src/edi/itunesquizz/kursordner.py
+++ src/edi/itunesquizz/kursordner.py
@@ -6,24 +6,13 @@ from plone.dexterity.browser import edit
 from plone.dexterity.browser import add
 from plone.dexterity.content import Container
 
-class IKursordner(Interface):
+class IKursordner(model.Schema):
     title = schema.TextLine(title=u"iTunes U Kursnummer")
+
     description = schema.Text(title=u"Kurzbeschreibung des Kurses", required=False)
+
     email = schema.TextLine(title=u'E-Mail-Adresse', description=u"An diese E-Mail-Adresse werden alle Ergebnisse der benoteten Aufgaben\
                                                                    geschickt")
 
 class Kursordner(Container):
     """Dexterity Class"""
-
-
-class EditForm(edit.DefaultEditForm):
-    fields = field.Fields(IKursordner)
-
-
-class AddForm(add.DefaultAddForm):
-    portal_type = u"Kursordner"
-    fields = field.Fields(IKursordner)
-
-
-class AddView(add.DefaultAddView):
-    form = AddForm

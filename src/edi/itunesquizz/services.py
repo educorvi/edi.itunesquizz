d80d527c58160f3e11f59adbbf415bcfef5d6b57
Lars Walther <meinkraeks@gmail.com>
2019-08-09 14:28:06 +0200

    mein Test-Service

--- src/edi/itunesquizz/services.py
+++ src/edi/itunesquizz/services.py
@@ -3,5 +3,19 @@ from plone.rest import Service
 class ValidateAufgabe(Service):
 
     def render(self):
+        data = self.request.get('data')
+        result = True
+        solution = []
+        for i in self.context.antworten:
+            if i.get('bewertung') == 'richtig':
+                solution.append(True)
+                if not self.context.antworten.index(i) in data:
+                    result = False
+            else:
+                solution.append(False)
+                if self.context.antworten.index(i) in data:
+                    result = False
         import pdb;pdb.set_trace()
-        return '{"message": "Hello World!"}'
+        retdict = {'result':result,
+                   'solution':solution}
+        return str(retdict)
